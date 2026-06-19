a8550d56cd2bdbbc28534d423c015c504d50b865
YidaBao10 <byd298895661@gmail.com>
2026-06-19 22:35:39 +0800

    Docs: Synchronize all local updates and math notes to cloud

--- Reinforcement_Learning强化学习/Stage1_Data_Stack_Prep/Task1_Env_Setup/Code/10.  hogwarts.py
+++ Reinforcement_Learning强化学习/Stage1_Data_Stack_Prep/Task1_Env_Setup/Code/10.  hogwarts.py
@@ -8,3 +8,8 @@ students = ["Hermione", "Harry", "Ron"]
 
 for i in range(len(students)):
     print(i+1, students[i])
+#%%
+students = ["Hermione", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(i+1, students[i])
